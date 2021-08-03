a290651a664f3645b02d0b5fac80625fc518271f
subham <subhamjyoti1@gmail.com>
2021-08-03 10:40:52 +0530

    DecissionTree Done

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -20,12 +20,39 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
-#########################################################
+##############################
 ### your code goes here ###
-
-
+length = len(features_train[0])
+print "length : " , length
+###########################
 #########################################################
 
+from sklearn import tree 
+from sklearn.metrics import accuracy_score
 
+
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+t0 = time()
+clf = clf.fit( features_train , labels_train )
+print "fitting time :",round((time()-t0),3),"s"
+
+t0 = time()
+pred = clf.predict( features_test )
+print "prediction time :",round((time()-t0),3),"s"
+
+t0 = time()
+accuracy = accuracy_score( pred , labels_test )
+print "accuracy time :",round((time()-t0),3),"s"
+
+print "accuracy : " , accuracy 
+
+
+#when selectPercentile is 1 
+#training time : 3.829 s
+#no . of fearures : 379
+# ----------------------------
+#when selectPercentile is 10
+#training time : 125.378 s 
+#no . of fearures : 3785

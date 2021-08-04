04cf70740c2b95894e32cc19f521cd3748c01d35
subham <subhamjyoti1@gmail.com>
2021-08-04 22:03:27 +0530

    94% accuracy knn

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -33,7 +33,9 @@ plt.show()
 
 from time import time 
 from sklearn.metrics import accuracy_score
+print "AdaBoost"
 from sklearn.ensemble import AdaBoostClassifier
+
 clf = AdaBoostClassifier(learning_rate = 0.5 ,n_estimators = 24)
 
 t0 = time()
@@ -50,6 +52,35 @@ acc = accuracy_score( pred , labels_test )
 print "Accuracy time :  " , round(time()-t0,3) , "s"
 print "Accuracy :" , acc 
 
+#---------------------------------------------------------------------
+print" "
+print"Knn"
+from sklearn.neighbors import KNeighborsClassifier
+clf_knn = KNeighborsClassifier(n_neighbors=4 )
+
+t0 = time()
+clf.knn = clf_knn.fit(features_train, labels_train)
+print "Training time : " , round(time()-t0,3) , "s"
+
+t0 = time()
+pred_knn = clf_knn.predict(features_test)
+print "Prediction time : " , round(time()-t0,3) , "s"
+print "Accuracy kNN:", accuracy_score(pred_knn , labels_test)
+
+#---------------------------------------------------------------------
+print" "
+print"Random forest"
+from sklearn.ensemble import RandomForestClassifier
+clf_rf = RandomForestClassifier(n_estimators=15, min_samples_split=6)
+
+t0 = time()
+clf_rf.fit(features_train, labels_train)
+print "Training time : " , round(time()-t0,3) , "s"
+
+t0 = time()
+clf_rf = clf_rf.predict(features_test)
+print "Prediction time : " , round(time()-t0,3) , "s"
+print "Accuracy random forests:", accuracy_score(labels_test, clf_rf)
 
 
 try:
@@ -58,6 +89,13 @@ except NameError:
     pass
 
 
+
+#overal--
+#best model for above : knn 
+#accuracy : 94%
+
+
+#--------------------------------adaboost
 #performance to beat: 93.6
 #-------------------
 #Till Now Best : 0.928 
@@ -71,4 +109,22 @@ except NameError:
                 # learning_rate =0.8  : 0.924
                 # learning_rate =0.1 : 0.916
     #result : max at :1
-#
+#-----------------------------------KNN
+
+#phase 2
+
+# AdaBoost
+# Training time :  0.031 s
+# Prediction time :  0.015 s
+# Accuracy time :   0.0 s
+# Accuracy : 0.928
+
+# Knn
+# Training time :  0.0 s
+# Prediction time :  0.0 s
+# Accuracy kNN: 0.94
+
+# Random forest
+# Training time :  0.032 s
+# Prediction time :  0.0 s
+# Accuracy random forests: 0.92
